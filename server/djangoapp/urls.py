2551d53ef07673df93a52183bd546114ed03754b
nguyennguyen0110 <nguyennguyen0110@gmail.com>
2021-07-09 04:33:14 +0000

    temp change

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 from . import views
+from django.contrib import admin
 
 app_name = 'djangoapp'
 urlpatterns = [
@@ -10,8 +11,10 @@ urlpatterns = [
     # name the URL
 
     # path for about view
+    path(route='about/', view=views.about, name='about'),
 
     # path for contact us view
+    path(route='contact/', view=views.contact, name='contact'),
 
     # path for registration
 
@@ -19,7 +22,9 @@ urlpatterns = [
 
     # path for logout
 
+
     path(route='', view=views.get_dealerships, name='index'),
+    path('admin/', admin.site.urls),
 
     # path for dealer reviews view
 
