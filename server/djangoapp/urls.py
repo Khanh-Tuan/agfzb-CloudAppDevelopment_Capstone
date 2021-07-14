332d32b68a46d46dc6f411dfd334131754a23f76
nguyennguyen0110 <85159841+nguyennguyen0110@users.noreply.github.com>
2021-07-14 10:57:53 +0700

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -33,9 +33,9 @@ urlpatterns = [
 
 
     # path for dealer reviews view
-    path('dealer/', views.get_dealer_details, name='dealer_details'),
+    path('dealer/', view=views.get_dealer_details, name='dealer_details'),
 
     # path for add a review view
-    path('review/', views.add_review, name='add_review'),
+    path('review/', view=views.add_review, name='add_review'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
