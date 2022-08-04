f2a053b0b6dd7dc6a592f115405f62f17914b87a
Khanh-Tuan <107778275+Khanh-Tuan@users.noreply.github.com>
2022-08-05 00:10:40 +1200

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -14,12 +14,11 @@ urlpatterns = [
     # path for contact us view
  path(route='contact/', view=views.contact, name='contact'),
     # path for registration
- path(route='registration', view=views.registration, name='registration')
+ path('registration/', views.registration_request, name='registration'),
     # path for login
 
     # path for logout
-
-    path(route='', view=views.get_dealerships, name='index'),
+ path(route='', view=views.get_dealerships, name='index'),
 
     # path for dealer reviews view
 
