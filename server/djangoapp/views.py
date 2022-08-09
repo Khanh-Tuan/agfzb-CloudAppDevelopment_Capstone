434c5ed4842dff6e8496e07938463d9f7fd77741
Khanh-Tuan <107778275+Khanh-Tuan@users.noreply.github.com>
2022-08-09 23:10:50 +1200

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,137 +1,52 @@
-from django.shortcuts import render
-from django.http import HttpResponseRedirect, HttpResponse
-from django.contrib.auth.models import User
-from django.shortcuts import get_object_or_404, render, redirect
-# from .models import related models
-from .models import CarMake, CarModel
-# from .restapis import related methods
-from .restapis import get_dealers_from_cf, get_dealer_reviews_from_cf, post_request
-from django.contrib.auth import login, logout, authenticate
-from django.contrib import messages
-from datetime import datetime
-import logging
-import json
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    {% load static %}
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.5.0/css/bootstrap-datepicker.css" rel="stylesheet">
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.5.0/js/bootstrap-datepicker.js"></script>
 
-# Get an instance of a logger
-logger = logging.getLogger(__name__)
-
-
-# Create your views here.
-
-
-# Create an `about` view to render a static about page
-def about(request):
-    context = {}
-    if request.method == "GET":
-        return render(request, 'djangoapp/about.html', context)
-
-# Create a `contact` view to return a static contact page
-def contact(request):
-    context = {}
-    if request.method == "GET":
-        return render(request, 'djangoapp/contact.html', context)
-
-# Create a `login_request` view to handle sign in request
-def login_request(request):
-    context = {}
-    if request.method == "POST":
-        username = request.POST['username']
-        password = request.POST['psw']
-        user = authenticate(username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('djangoapp:index')
-        else:
-            context['message'] = "Invalid username or password."
-            return render(request, 'djangoapp/index.html', context)
-    else:
-        return render(request, 'djangoapp/index.html', context)
-
-
-# Create a `logout_request` view to handle sign out request
-def logout_request(request):
-    logout(request)
-    return redirect('djangoapp:index')
-
-# Create a `registration_request` view to handle sign up request
-def registration_request(request):
-    context = {}
-    if request.method == 'GET':
-        return render(request, 'djangoapp/registration.html', context)
-    elif request.method == 'POST':
-        # Check if user exists
-        username = request.POST['username']
-        password = request.POST['psw']
-        first_name = request.POST['firstname']
-        last_name = request.POST['lastname']
-        user_exist = False
-        try:
-            User.objects.get(username=username)
-            user_exist = True
-        except:
-            logger.error("New user")
-        if not user_exist:
-            user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name,
-                                            password=password)
-            login(request, user)
-            return redirect("djangoapp:index")
-        else:
-            context['message'] = "User already exists."
-            return render(request, 'djangoapp/registration.html', context)
-
-# Update the `get_dealerships` view to render the index page with a list of dealerships
-def get_dealerships(request):
-    context = {}
-    if request.method == "GET":
-        url = "https://67a392c1.us-south.apigw.appdomain.cloud/api/dealerships"
-        # Get dealers from the URL
-        dealerships = get_dealers_from_cf(url)
-        context = {}
-        context['dealerships'] = dealerships
-        # Return a list of dealer short name
-        return render(request, 'djangoapp/index.html', context)
-        
-# Create a `get_dealer_details` view to render the reviews of a dealer
-def get_dealer_details(request, dealer_id):
-    context = {}
-    if request.method == "GET":
-        url = "https://67a392c1.us-south.apigw.appdomain.cloud/api/reviews"
-        # Get dealers from the URL
-        reviews = get_dealer_reviews_from_cf(url, dealer_id = dealer_id)
-        # Append to context
-        context['reviews'] = reviews
-        context['dealer_id'] = dealer_id
-        # Return a list of dealer short name
-        return render(request, 'djangoapp/dealer_details.html', context)
-
-# Create a `add_review` view to submit a review
-def add_review(request, dealer_id):
-    user = request.user
-    context = {}
-    if request.method == "GET":
-        cars = CarModel.objects.filter(dealer_id=dealer_id)
-        context['cars'] = cars
-        context['dealer_id'] = dealer_id
-        return render(request, 'djangoapp/add_review.html', context)
-    elif request.method == "POST":
-        if user.is_authenticated:
-            url = 'https://67a392c1.us-south.apigw.appdomain.cloud/api/reviews'
-            review = {}
-            review['name'] = user.first_name + ' ' + user.last_name
-            review['dealership'] = dealer_id
-            review['review'] = request.POST['content']
-            review['purchase'] = request.POST.get("purchasecheck") == 'on'
-            if request.POST.get("purchasecheck") == 'on':
-                review['purchase_date'] = request.POST["purchasedate"]
-                car = CarModel.objects.get(pk=request.POST["car"])
-                review['car_make'] = car.carmake.name
-                review['car_model'] = car.name
-                review['car_year'] = car.year.strftime("%Y")
-            json_payload = {}
-            json_payload['review'] = review
-            print (json_payload)
-            result = post_request(url, json_payload, dealerId=dealer_id)
-            print ('POST result: ')
-            print (result)
-            context["dealer_id"] = dealer_id
-            return redirect("/djangoapp/dealer/" + str(dealer_id), context)
+</head>
+    <body>
+    <!--Add review form here -->
+    <form action="{% url 'djangoapp:add_review' dealer_id %}" method="post">
+        {% csrf_token %}
+        <br>
+        <div class="form-group" style='padding: 20px'>
+            <label for="content"> Your review: </label>
+            <textarea class="form-control" id="content" name="content" rows="2" required></textarea>
+        </div>
+        <br>
+        <div class="form-group form-check" style='padding: 20px'>
+            <input class="form-check-input" type="checkbox" name="purchasecheck" id="purchasecheck" style='padding: 20px'>
+            <label for="purchasecheck"> Did you buy car from this dealership? (Check if yes) </label>
+        </div>
+        <br>
+        <div class="form-group" style='padding: 20px'>
+            <label for="car">  Select the car you bought (model-brand-year):</label>
+            <br>
+            <select name="car" id="car" class="form-select">
+                {% for car in cars %}
+                    <option selected value={{car.id}}>{{car.name}}-{{car.carmake.name}}-{{ car.year|date:"Y" }}</option>
+                {% endfor %}
+            </select>
+        </div>
+        <br>
+        <div class="form-group" style='padding: 20px'>
+            <label for="purchasedate"> When did you buy it? </label>
+            <input class="date-own form-control" type="text" name="purchasedate" id="purchasedate">
+        </div>
+        <br><br>
+        <button type="submit" class="btn btn-primary" style='padding: 20px'>Submit</button>
+        <br><br>
+        <script type="text/javascript">
+            $('.date-own').datepicker({
+                format: 'mm/dd/yyyy'
+            });
+        </script>
+    </form>
+    </body>
+</html>
